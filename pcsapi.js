cda7a6837c236bd6b9cb088fac700df625ddfc1f
Yangff <yangff1@gmail.com>
2013-09-16 20:35:10 +0800

    Update pcsapi.js

--- pcsapi.js
+++ pcsapi.js
@@ -4,9 +4,9 @@ var pcshelper = {
     download : function (key){
         //http://pan.baidu.com/api/list?channel=chunlei&clienttype=0&web=1&num=100&t=1379255798845&page=1&dir=%2Fgeekcollege&t=0.25950&order=time&desc=1
         //var akey = key.split('.');
-        //return {'url':('http://pan.baidu.com/api/search?&dir=&web=1&key=' + encodeURIComponent(akey[2] + "." + akey[3] + ".gkv.mp4") + '&recursion&timeStamp=' + Math.random())};
+        //return {'url':('http://pan.baidu.com/api/search?&dir=&web=1&key=' + encodeURIComponent(akey[2] + "." + akey[3] + ".mp4") + '&recursion&timeStamp=' + Math.random())};
         var akey = key.split('.');
-        var vp = '/geekcollege/' + akey[2] + "/" + akey[1] + '/' + akey[0] + '/';
+        var vp = '/node/' + akey[2] + "/" + akey[1] + '/' + akey[0] + '/';
         return {'url':
             "http://pan.baidu.com/api/list?channel=chunlei&clienttype=0&web=1&num=100&t=" + Date.now() + "&page=1&dir=" + encodeURIComponent(vp) + "&t=" + Math.random() + "&order=time&desc=1&_=" + Date.now()
         }
@@ -14,15 +14,15 @@ var pcshelper = {
     upload : function (key){
         var akey = key.split('.');
         //console.log(key.replace(/\./g,'/'));
-        var vp = '/geekcollege/' + akey[2] + "/" + akey[1] + '/' + akey[0] + '/';
-        var path = encodeURIComponent(vp + akey[3] + ".gkv.mp4");
+        var vp = '/node/' + akey[2] + "/" + akey[1] + '/' + akey[0] + '/';
+        var path = encodeURIComponent(vp + akey[3] + ".mp4");
         var cfg = {
             "content-length":akey[0],
             "content-md5":akey[2],
             "slice-md5":akey[1],
             "contentcrc32":akey[3]
         };
-        //https://pcs.baidu.com/rest/2.0/pcs/file?method=rapidupload&app_id=250528&path=%2F%E6%B5%8B%E8%AF%95%E5%BA%94%E7%94%A8%2F91ad999814895e979c79be9bca9882f9.gkv&content-length=148645974&content-md5=91ad999814895e979c79be9bca9882f9&slice-md5=b60cc48eef06505537936fd3a66b0344&contentcrc32=1373590915
+        //https://pcs.baidu.com/rest/2.0/pcs/file?method=rapidupload&app_id=250528&path=%2F%E6%B5%8B%E8%AF%95%E5%BA%94%E7%94%A8%2F91ad999814895e979c79be9bca9882f9&content-length=148645974&content-md5=91ad999814895e979c79be9bca9882f9&slice-md5=b60cc48eef06505537936fd3a66b0344&contentcrc32=1373590915
         return {url:('https://pcs.baidu.com/rest/2.0/pcs/file?method=rapidupload&app_id=250528&path=' + path),'post':cfg};
     }
 };
